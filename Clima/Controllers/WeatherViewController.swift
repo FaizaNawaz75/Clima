f6b566575133c7ff48d0bb252b828410aa645f08
Faiza Nawaz <Faizanawaz755@gmail.com>
2023-04-06 17:47:28 +0500

    Practiced stack views, appearances (light/dark mode), color sets, vectors

---
+++ Clima/Controllers/WeatherViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Faiza Nawaz on 06/04/2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var lbCity: UILabel!
+    @IBOutlet weak var lbTemprature: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
