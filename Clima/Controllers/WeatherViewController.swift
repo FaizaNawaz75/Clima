afbf8ce5116a344e3c627026f1d32dd4ec4a1116
Faiza Nawaz <Faizanawaz755@gmail.com>
2023-04-07 15:00:39 +0500

    handled text field methods

--- Clima/Controllers/WeatherViewController.swift
+++ Clima/Controllers/WeatherViewController.swift
@@ -12,11 +12,47 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var lbCity: UILabel!
     @IBOutlet weak var lbTemprature: UILabel!
     @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var tfSearch: UITextField!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        tfSearch.delegate = self
     }
 
-
+    //MARK: - Action methods
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+        performSearch()
+    }
+    
+    fileprivate func performSearch() {
+        
+        tfSearch.endEditing(true)
+    }
 }
 
+extension WeatherViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        performSearch()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+       
+        textField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
+        if (textField.text != "") {
+            return true
+        }
+        
+        textField.placeholder = "Type Something"
+        return false
+    }
+}
